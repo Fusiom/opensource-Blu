3aa9e9587c381e93d38f8ce0158677633d8e0962
NoKil <85508081+nokil1@users.noreply.github.com>
2022-03-16 16:51:49 +0000

    deez

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import git
 
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+
 client = commands.Bot(command_prefix = "+")
 
 slash = SlashCommand(client,sync_commands=True)
+
+@client.event
+async def on_ready():
+	print('We have logged in as {0.user}'.format(client))
+	await client.change_presence(status=discord.Status.idle, activity=discord.Game('$help'))
 
 repo = git.Repo("./")
 # pull down 
