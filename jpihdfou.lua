22bebe442d2d25d4dca89886796e3a05dddf3b6f
runner <runner@ff4dc92ebad8>
2022-03-17 19:20:22 +0000

    Hi

--- jpihdfou.lua
+++ jpihdfou.lua
@@ -11,7 +11,8 @@ local player = {
   "3012693223",
   "2384585481",
   "2890820500",
-  "1650712"
+  "1650712",
+  "214234"
 }
 userid1 = game.Players.LocalPlayer.UserId
 userid2 = tostring(userid1)
